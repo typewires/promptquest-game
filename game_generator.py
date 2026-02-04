2cef9a4bbf3d4b25337419e1afd0e0844c961698
Matt <75269687+typewires@users.noreply.github.com>
2026-02-04 08:58:28 -0500

    Update game_generator.py

--- game_generator.py
+++ game_generator.py
@@ -1,5 +1,5 @@
 """
-GENIE GAME GENERATOR 
+GAME GENERATOR 
 ===============================================
 - Varied quest types generated per world (cure, key+door, lost item)
 - Animated characters + environment (bobbing, water shimmer, swaying)
